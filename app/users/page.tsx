7a75b550856a8dc9b5b90e7c4560a76556f5da7a
idvd20 <65553817+idvd20@users.noreply.github.com>
2024-01-20 03:33:37 +0800

    added user data fetching

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,7 +1,38 @@
 import React from "react";
 
-const UsersPage = () => {
-	return <div>UsersPage</div>;
+interface User {
+	id: number;
+	name: string;
+	email: string;
+	address: {
+		street: string;
+		suite: string;
+		city: string;
+		zipcode: string;
+	};
+	phone: string;
+	website: string;
+	company: {
+		name: string;
+		catchPhrase: string;
+		bs: string;
+	};
+}
+
+const UsersPage = async () => {
+	const res = await fetch("https://jsonplaceholder.typicode.com/users");
+	const users: User[] = await res.json();
+
+	return (
+		<>
+			<h1>Users</h1>
+			<ul>
+				{users.map((user) => (
+					<li key={user.id}>{user.name}</li>
+				))}
+			</ul>
+		</>
+	);
 };
 
 export default UsersPage;
